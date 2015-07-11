73e2965c7fab3ae8dd4b7eda73be60035aa31f53
Gary Cuga-Moylan <gary@telegraphicsinc.com>
2015-07-11 17:33:38 -0500

    Working out kinks in grid system

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Portfolio of Chicago based Front-End Web Developer, Gary Cuga-Moylan">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Gary Cuga-Moylan's Portfolio</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md">
+          <h2>Lorem Ipsum</h2>
+        </div>
+        <div class="col-md">
+          <h2>Lorem Ipsum</h2>
+        </div>
+        <div class="col-md">
+          <h2>Lorem Ipsum</h2>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
